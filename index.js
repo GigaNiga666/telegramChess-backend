47883a2377180725f4e855297e78757528e82aaa
Stanislav <stasbugaev2006@gmail.com>
2023-08-12 23:00:43 +0500

    init

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const cors = require('cors')
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors : {origin : 'https://bejewelled-sundae-b83073.netlify.app'}})
+const io = require('socket.io')(server, {cors : {origin : 'https://2427-79-172-16-96.ngrok-free.app/'}})
 const initClient = require('./gameLogic')
 
 const PORT = process.env.PORT && 5000
 
 app.use(cors({
-    origin: 'https://bejewelled-sundae-b83073.netlify.app/'
+    origin: 'https://2427-79-172-16-96.ngrok-free.app/'
 }))
 
 app.get('/', (req, res) => {
