0539740a676cc1b992b5882e01405eca3c798021
Stanislav <stasbugaev2006@gmail.com>
2023-08-12 23:06:24 +0500

    init

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const cors = require('cors')
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors : {origin : 'https://2427-79-172-16-96.ngrok-free.app/'}})
+const io = require('socket.io')(server, {cors : {origin : 'https://telegram-chess.vercel.app'}})
 const initClient = require('./gameLogic')
 
 const PORT = process.env.PORT && 5000
 
 app.use(cors({
-    origin: 'https://2427-79-172-16-96.ngrok-free.app/'
+    origin: 'https://telegram-chess.vercel.app'
 }))
 
 app.get('/', (req, res) => {
