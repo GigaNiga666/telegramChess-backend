e613a7fa5bcf5b7fdbc0d4bcf01114789151667d
Stanislav <stasbugaev2006@gmail.com>
2023-08-15 16:19:36 +0500

    fix

--- telegramBot.js
+++ telegramBot.js
@@ -6,6 +6,14 @@ function initTelegramBot() {
     bot = new TelegramApi(process.env.TOKEN, {polling:true})
 
     bot.on('message', handleMessageText)
+    bot.on('callback', handleCallback)
+}
+async function handleCallback(callback) {
+    const chatId = callback.user.chat.id
+
+    if (callback.data === 'connectToRoom') {
+        await bot.sendMessage(chatId, 'Напиши username пользователя к которому хочешь подключиться')
+    }
 }
 
 async function handleMessageText(msg) {
@@ -13,16 +21,23 @@ async function handleMessageText(msg) {
     const chatId = msg.chat.id
 
     if (msgText === '/start') {
-        await bot.sendMessage(chatId, 'Здоровей будешь!', {
+        await bot.sendMessage(chatId, 'Выбери 1 из действий:', {
             reply_markup: {
                 inline_keyboard: [
-                    [{text: 'Chess', web_app: {url: `https://telegram-chess.vercel.app/game/${msg.from.username}`}}]
+                    [{text: 'Создать комнату', web_app: {url: `https://telegram-chess.vercel.app/game/${msg.from.username}`}}],
+                    [{text: 'Подключиться к комнате', value : 'connectToRoom', web_app: {url: `https://telegram-chess.vercel.app/game/${msg.from.username}`}}]
                 ]
             }
         })
     }
     else {
-        await bot.sendMessage(chatId, msgText)
+        await bot.sendMessage(chatId, 'Подключиться к комнате пользователя:', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Подключиться к комнате', value : 'connectToRoom', web_app: {url: `https://telegram-chess.vercel.app/game/${msgText}`}}]
+                ]
+            }
+        })
     }
 }
 
