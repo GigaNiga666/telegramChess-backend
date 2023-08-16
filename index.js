24c22d4218bfe37b7c0c1f8c3f4f51e6ae513a84
Stanislav <stasbugaev2006@gmail.com>
2023-08-16 13:51:10 +0500

    fix

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const cors = require('cors')
-const app = require('express')()
+const express = require("express");
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {cors : {origin : process.env.FRONTEND_URL}})
 const initClient = require('./gameLogic')
 const bot = require('./telegramBot')()
 
 const PORT = process.env.PORT && 5000
+const app = express()
 
 app.use(cors({
     origin: process.env.FRONTEND_URL
 }))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json('Server work')
